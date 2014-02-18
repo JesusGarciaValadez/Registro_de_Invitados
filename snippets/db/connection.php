021dcb424f8f436923c46aeb75443ca1e515f71e
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2014-02-18 09:59:01 -0600

    ignoring connection files from commit
    Signed-off-by: Jesús Antonio García Valadez <zerovamp@hotmail.com>

--- snippets/db/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=registry_guest', 'user', 'password');
-    $dbh->exec("SET CHARACTER SET utf8");    
-}
-catch ( PDOException $e ){
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
